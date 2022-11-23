c0330aefdab0e5797474b16baeed2c38d30b36b5
eliszewski <liszewski31098@gmail.com>
2022-11-23 13:15:08 -0500

    add imports for animal shelters

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/ChickenCoop.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.froilansfarm.Shelter;
 
+import com.zipcodewilmington.froilansfarm.Chicken;
+
 public class ChickenCoop extends Shelter<Chicken> {
     public ChickenCoop() {
     }
