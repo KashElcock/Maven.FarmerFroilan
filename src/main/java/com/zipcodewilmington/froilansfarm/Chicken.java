dc830ab7e71c015f525a6be382f1d3b1841637cc
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-23 13:09:03 -0500

    added Animal, Chicken, and Horse as well as tests.

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Chicken extends Animal{
+    //not fertilized egg adds to population of chicken
+    //fertilized egg turns to edible object
+
+    public void layEgg(){}
+    public void yieldEgg(){}
+//    @Override
+//    public void eat(Edible corn){
+//
+//    }
+
+
+}
+//chicken can eat edibles of type corn and make noise
