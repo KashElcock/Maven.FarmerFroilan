a232577f872fe2890d043b26e5fe20146b1322be
eliszewski <liszewski31098@gmail.com>
2022-11-25 13:37:41 -0500

    add feed horses and tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Stable.java
@@ -1,8 +1,26 @@
 package com.zipcodewilmington.froilansfarm.Shelter;
 
+import com.zipcodewilmington.froilansfarm.EarCorn;
 import com.zipcodewilmington.froilansfarm.Horse;
 
 public class Stable extends Shelter<Horse> {
     public Stable() {
+    }
+
+    @Override
+    public String toString() {
+        return "Stable{" +
+                "modCount=" + modCount +
+                '}';
+    }
+
+    public void feedHorses(EarCorn... earCorns) {
+        for (Horse horse: this
+             ) {
+            for (EarCorn earCorn: earCorns
+                 ) {
+                horse.eat(earCorn);
+            }
+        }
     }
 }
